0dedd893e39206ea6f0632a3a7873b5b4b1f003b
Paulo Fernando <paulo.fdo@hotmail.com>
2024-10-29 21:47:22 -0300

    Cria projeto + Input e Output Paths

---
+++ EdmxToFluentApi/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace EdmxToFluentApi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var edmxInputPath = string.Empty;
+            var fluentMappingOutputPath = string.Empty;
+
+            while (string.IsNullOrEmpty(edmxInputPath) || !File.Exists(edmxInputPath))
+            {
+                Console.Write("EDMX input path: ");
+                edmxInputPath = Console.ReadLine();
+            }
+
+            while (string.IsNullOrEmpty(fluentMappingOutputPath) || !Directory.Exists(fluentMappingOutputPath))
+            {
+                Console.Write("Fluent Mapping output path: ");
+                fluentMappingOutputPath = Console.ReadLine();
+            }
+
+            Console.WriteLine("Press some key to exit >>>");
+            Console.ReadKey();
+        }
+    }
+}
